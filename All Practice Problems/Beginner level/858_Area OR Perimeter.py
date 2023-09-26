4daa002b153e28789ca00afb6cc7d39ad806ec79
sampada109 <143282629+sampada109@users.noreply.github.com>
2023-09-26 21:50:58 +0530

    Update 858_Area OR Perimeter.py

--- All Practice Problems/Beginner level/858_Area OR Perimeter.py
+++ All Practice Problems/Beginner level/858_Area OR Perimeter.py
@@ -3,8 +3,8 @@ b = int(input())
 area = a*b 
 peri = 2*(a+b)
 if area>peri:
-    print('Area')
+    print('Area\n',area)
 elif area==peri:
-    print('Equal')
+    print('Eq\n',area)
 else:
-    print('Peri')
+    print('Peri\n',peri)
